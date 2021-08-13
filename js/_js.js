de074519aed0b61a31ba427f31a7065eed0439c4
jackbate <75363110+jackbate@users.noreply.github.com>
2021-08-13 23:53:50 +0100

    Addition of carousel and many bug fixes and visual fixes

---
+++ js/_js.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+//     $('.carousel').slick({
+//       setting-name: setting-value
+//     });
+//   });
+
+function myFunction(x) {
+    x.classList.toggle("change");
+} 
+  
