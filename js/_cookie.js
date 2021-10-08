6cfff45d47428e9401898e34fc8e5a8458ac93e3
jackbate <75363110+jackbate@users.noreply.github.com>
2021-10-08 01:36:36 +0100

    cookie reflection change along side other fixes

--- js/_cookie.js
+++ js/_cookie.js
@@ -1,28 +1,28 @@
 
 
-(function() {
-	if (!localStorage.getItem('cookieconsent')) {
-		var request = new XMLHttpRequest();
-		request.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				var data = JSON.parse(request.responseText);
-				var eu_country_codes = ['AL','AD','AM','AT','BY','BE','BA','BG','CH','CY','CZ','DE','DK','EE','ES','FO','FI','FR','GB','GE','GI','GR','HU','HR','IE','IS','IT','LT','LU','LV','MC','MK','MT','NO','NL','PO','PT','RO','RU','SE','SI','SK','SM','TR','UA','VA'];
-				if (eu_country_codes.indexOf(data.countryCode) != -1) {
-					document.body.innerHTML += '\
-					<div class="cookieconsent">\
-                    This site uses cookies. By continuing to use this website, you agree to their use. \
-                    <a href="#" class="cookie-btn">I Understand</a>\
-                    </div>\
-					';
-					document.querySelector('.cookieconsent a').onclick = function(e) {
-						e.preventDefault();
-                        document.querySelector('.cookieconsent').style.display = 'none';
-						localStorage.setItem('cookieconsent', true);
-					};
-				}
-			}
-		};
-		request.open('GET', 'http://ip-api.com/json', true);
-		request.send();
-	}
-})();
+// (function() {
+// 	if (!localStorage.getItem('cookieconsent')) {
+// 		var request = new XMLHttpRequest();
+// 		request.onreadystatechange = function() {
+// 			if (this.readyState == 4 && this.status == 200) {
+// 				var data = JSON.parse(request.responseText);
+// 				var eu_country_codes = ['AL','AD','AM','AT','BY','BE','BA','BG','CH','CY','CZ','DE','DK','EE','ES','FO','FI','FR','GB','GE','GI','GR','HU','HR','IE','IS','IT','LT','LU','LV','MC','MK','MT','NO','NL','PO','PT','RO','RU','SE','SI','SK','SM','TR','UA','VA'];
+// 				if (eu_country_codes.indexOf(data.countryCode) != -1) {
+// 					document.body.innerHTML += '\
+// 					<div class="cookieconsent">\
+//                     This site uses cookies. By continuing to use this website, you agree to their use. \
+//                     <a href="#" class="cookie-btn">I Understand</a>\
+//                     </div>\
+// 					';
+// 					document.querySelector('.cookieconsent a').onclick = function(e) {
+// 						e.preventDefault();
+//                         document.querySelector('.cookieconsent').style.display = 'none';
+// 						localStorage.setItem('cookieconsent', true);
+// 					};
+// 				}
+// 			}
+// 		};
+// 		request.open('GET', 'http://ip-api.com/json', true);
+// 		request.send();
+// 	}
+// })();
